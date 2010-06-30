a7863b5c0ee7be1a3976077768a9bf25cae47f7e
Lorenz Cuno Klopfenstein <lck@klopfenstein.net>
2010-06-30 13:42:35 +0200

    Refactored Hot Key processing and Dual Mode as extensible Message Pump Processors.

---
+++ OnTopReplica/MessagePumpManager.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Reflection;
+using System.Windows.Forms;
+
+namespace OnTopReplica {
+    class MessagePumpManager : IDisposable {
+
+        Dictionary<Type, IMessagePumpProcessor> _processors = new Dictionary<Type, IMessagePumpProcessor>();
+
+        /// <summary>
+        /// Instantiates all message pump processors and registers them on the main form.
+        /// </summary>
+        /// <param name="form"></param>
+        public void Initialize(MainForm form) {
+            foreach (var t in Assembly.GetExecutingAssembly().GetTypes()) {
+                if (typeof(IMessagePumpProcessor).IsAssignableFrom(t) && !t.IsAbstract) {
+                    var instance = (IMessagePumpProcessor)Activator.CreateInstance(t);
+                    instance.Initialize(form);
+
+                    _processors.Add(t, instance);
+                    
+                    Console.WriteLine("Registered message pump processor: {0}", t);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Run the registered message pump processors.
+        /// </summary>
+        /// <param name="msg">Message to process.</param>
+        public void PumpMessage(Message msg) {
+            foreach (var processor in _processors.Values) {
+                processor.Process(msg);
+            }
+        }
+
+        /// <summary>
+        /// Get the instance of a registered message pump processor.
+        /// Throw if instance not found.
+        /// </summary>
+        public T Get<T>() {
+            return (T)_processors[typeof(T)];
+        }
+
+        #region IDisposable Members
+
+        public void Dispose() {
+            foreach (var processor in _processors.Values) {
+                processor.Dispose();
+            }
+            _processors.Clear();
+        }
+
+        #endregion
+    }
+}
---
+++ OnTopReplica/MessagePumpProcessors/HotKeyManager.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+using OnTopReplica.Native;
+
+namespace OnTopReplica.MessagePumpProcessors {
+
+    /// <summary>
+    /// HotKey registration helper.
+    /// </summary>
+    class HotKeyManager : BaseMessagePumpProcessor {
+
+        int _lastUsedKey = 0;
+
+        Dictionary<int, HotKeyMethods.HotKeyHandler> _handlers = new Dictionary<int, HotKeyMethods.HotKeyHandler>();
+
+        public void RegisterHotKey(HotKeyModifiers mod, Keys keys, HotKeyMethods.HotKeyHandler handler) {
+            var newKey = ++_lastUsedKey;
+
+            if (!HotKeyMethods.RegisterHotKey(Form.Handle, newKey, (int)mod, (int)keys)) {
+                Console.Error.WriteLine("Failed to register {0} hot key.", newKey);
+                return;
+            }
+
+            _handlers[newKey] = handler;
+        }
+
+        public override void Process(Message msg) {
+            if (msg.Msg == HotKeyMethods.WM_HOTKEY) {
+                int keyId = msg.WParam.ToInt32();
+                if (!_handlers.ContainsKey(keyId))
+                    return;
+
+                _handlers[keyId].Invoke();
+            }
+        }
+
+        protected override void Shutdown() {
+            foreach (var key in _handlers.Keys) {
+                if (!HotKeyMethods.UnregisterHotKey(Form.Handle, key)) {
+                    Console.Error.WriteLine("Failed to unregister {0} hot key.", key);
+                }
+            }
+        }
+    }
+
+}
