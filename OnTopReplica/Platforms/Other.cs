ba1b2f9cb6d9a8ad0587c9d5d328d2d974f76109
Lorenz Cuno Klopfenstein <lck@klopfenstein.net>
2010-07-01 13:19:45 +0200

    Added error message for Other platform (same as Win XP at the time).

--- OnTopReplica/Platforms/Other.cs
+++ OnTopReplica/Platforms/Other.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Windows.Forms;
 
 namespace OnTopReplica.Platforms {
     class Other : PlatformSupport {
         
         public override bool CheckCompatibility() {
-            //TODO: Err MSG
+            //TODO: better error message
+            MessageBox.Show(Strings.ErrorNoDwm, Strings.ErrorNoDwmTitle, MessageBoxButtons.OK, MessageBoxIcon.Error);
             return false;
         }
 
