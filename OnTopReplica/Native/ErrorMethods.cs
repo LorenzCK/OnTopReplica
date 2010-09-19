1c209b46a5a6e0f6c250d7f9185d1eb8a8bbfd8a
Lorenz Cuno Klopfenstein <lck@klopfenstein.net>
2010-09-19 23:47:47 +0200

    Added program version to crash dump.

---
+++ OnTopReplica/Native/ErrorMethods.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Runtime.InteropServices;
+
+namespace OnTopReplica.Native {
+    /// <summary>
+    /// Common Win32 error handling methods.
+    /// </summary>
+    static class ErrorMethods {
+
+        [DllImport("kernel32.dll")]
+        static extern uint FormatMessage(uint dwFlags, IntPtr lpSource,
+           int dwMessageId, uint dwLanguageId, [Out] StringBuilder lpBuffer,
+           uint nSize, IntPtr Arguments);
+
+        /// <summary>
+        /// Gets a string representation of a Win32 error code.
+        /// </summary>
+        /// <param name="msgCode">ID of the Win32 error code.</param>
+        /// <returns>String representation of the error.</returns>
+        public static string GetErrorMessage(int msgCode) {
+            var sb = new StringBuilder(300);
+            FormatMessage((uint)(0x00001000), IntPtr.Zero, msgCode, 0, sb, 299, IntPtr.Zero);
+            return sb.ToString();
+        }
+
+        /// <summary>
+        /// Gets a string representation of the last Win32 error on this thread.
+        /// </summary>
+        public static string GetLastErrorMessage() {
+            int errorCode = Marshal.GetLastWin32Error();
+            return GetErrorMessage(errorCode);
+        }
+
+    }
+}
---
+++ OnTopReplica/Native/WindowsSevenMethods.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Runtime.InteropServices;
+
+namespace OnTopReplica.Native {
+    
+    static class WindowsSevenMethods {
+
+        [DllImport("shell32.dll")]
+        internal static extern void SetCurrentProcessExplicitAppUserModelID(
+            [MarshalAs(UnmanagedType.LPWStr)] string AppID);
+
+        [DllImport("shell32.dll")]
+        internal static extern void GetCurrentProcessExplicitAppUserModelID(
+            [Out(), MarshalAs(UnmanagedType.LPWStr)] out string AppID);
+
+    }
+
+}
