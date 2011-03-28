9c3ddfe95e5b802e829c1c153673b3604b22011a
Lorenz Cuno Klopfenstein <lck@klopfenstein.net>
2011-03-28 23:46:36 +0200

    Updated about panel as side panel, with updater.

--- OnTopReplica/SidePanels/AboutPanel.cs
+++ OnTopReplica/SidePanels/AboutPanel.cs
@@ -7,9 +7,14 @@ using System.Text;
 using System.Windows.Forms;
 using VistaControls.Dwm;
 using System.Diagnostics;
+using OnTopReplica.Update;
+using VistaControls.TaskDialog;
 
 namespace OnTopReplica.SidePanels {
     partial class AboutPanel : SidePanel {
+
+        UpdateManager _updater;
+
         public AboutPanel() {
             InitializeComponent();
 
@@ -17,6 +22,10 @@ namespace OnTopReplica.SidePanels {
             thlabelVersion.Text = string.Format(Strings.AboutVersion, Application.ProductVersion);
             InternationalizeLinkLabel(linkAuthor, Strings.AboutAuthor, Strings.AboutAuthorContent);
             InternationalizeLinkLabel(linkCredits, Strings.AboutCreditsSources, Strings.AboutCreditsSourcesContent);
+
+            //Updating
+            _updater = new UpdateManager();
+            _updater.UpdateCheckCompleted += new EventHandler<UpdateCheckCompletedEventArgs>(UpdateCheckCompleted);
         }
 
         private void InternationalizeLinkLabel(LinkLabel label, string text, string linkText) {
@@ -58,6 +67,31 @@ namespace OnTopReplica.SidePanels {
         }
 
         #endregion
+
+        void ButtonUpdate_click(object sender, System.EventArgs e) {
+            progressUpdate.Visible = true;
+
+            _updater.CheckForUpdate();
+        }
+
+        void UpdateCheckCompleted(object sender, UpdateCheckCompletedEventArgs e) {
+            this.Invoke(new Action(() => {
+                var topForm = this.TopLevelControl as Form;
+
+                if (e.Success) {
+                    _updater.HandleUpdateCheck(topForm, e.Information, true);
+                }
+                else {
+                    var dlg = new TaskDialog(Strings.ErrorUpdate, Strings.ErrorUpdate, Strings.ErrorUpdateContentGeneric) {
+                        CommonIcon = TaskDialogIcon.Stop,
+                        CommonButtons = TaskDialogButton.OK
+                    };
+                    dlg.Show(topForm);
+                }
+
+                progressUpdate.Visible = false;
+            }));
+        }
 
     }
 }
