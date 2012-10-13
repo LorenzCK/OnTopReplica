c2a61665d6398b2d92c6de40356e0535f695220e
Lorenz Cuno Klopfenstein <lck@klopfenstein.net>
2012-10-13 15:37:28 +0200

    Added Windows 8 support.

---
+++ OnTopReplica/Platforms/WindowsEight.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using OnTopReplica.Native;
+
+namespace OnTopReplica.Platforms {
+    class WindowsEight : WindowsSeven {
+
+        public override void PostHandleFormInit(MainForm form) {
+            //Noop
+        }
+
+    }
+}
