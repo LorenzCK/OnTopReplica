8aa867f41ba823e7db77b69e971576e3e3d88fa8
Lorenz Cuno Klopfenstein <lck@klopfenstein.net>
2013-10-23 16:22:11 +0200

    Initial commit of PostInstaller project.

---
+++ Installer/PostInstaller/PostInstaller/IShellItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace PostInstaller {
+    [ComImport]
+    [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+    [Guid("43826d1e-e718-42ee-bc55-a1e261c37bfe")]
+    public interface IShellItem {
+
+    }
+}
